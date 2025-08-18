8095a5d32253cc0b1846479f65c74ab1dffde335
TheFBOTI <martyn.b.se@gmail.com>
2025-08-18 14:11:37 +0100

    feat(no ticket): adding initial menu's that will be part of the terminal user experience

--- main.go
+++ main.go
@@ -1,1 +1,41 @@
-package WorldOfWarcraftTooling
+package main
+
+import (
+	"fmt"
+)
+
+func introduction() {
+	fmt.Println("Hello and welcome to the world of Warcraft local tooling")
+}
+
+var initialMenu = []string{
+	"Expansion",
+	"Exit",
+}
+
+var expansionMenu = []string{
+	"War Within",
+}
+
+var seasonMenu = []string{
+	"Season Three/ Season 3",
+}
+
+var dungeonOrRaidMenu = []string{
+	"Dungeons",
+	"Raid(s)",
+}
+
+var dungeonsMenu = []string{
+	"Halls of Attonement",
+}
+
+var raidMenu = []string{
+	"Manaforge Omega",
+}
+
+// TODO - can we prevent loading all the bosses as individual menus and upload them as files and load them in.
+
+func main() {
+	introduction()
+}
